c7c30cc4a0fd35183ca232f4f80ee9997bef853c
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-03-27 22:51:00 -0300

    Added social config

--- config/config.js
+++ config/config.js
@@ -4,6 +4,23 @@ module.exports = {
         pageSubTitle : 'An awesome page subtitle here.',
         aboutText    : 'About text here',
         email        : 'myemail@mysever.com',
-        access_token : '' //Your Instagram Access Token here
+        access_token : '', //Your Instagram Access Token here
+        social : [
+            {
+                network: 'facebook',
+                url: 'https://www.facebook.com/',
+                username: 'username'
+            },
+            {
+                network: 'twitter',
+                url: 'https://twitter.com/',
+                username: 'username'
+            },
+            {
+                network: 'instagram',
+                url: 'https://instagram.com/',
+                username: 'username'
+            }
+        ]
     }
 };
