579be80c30f03932636b6eb60762de3a12b94f73
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-08-29 14:32:28 -0300

    Changed application endpoint

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,11 @@ var express = require('express'),
     moment  = require('moment'),
     config  = require('../config/config.js'),
     router  = express.Router(),
-    user    = config.instagram.access_token.split('.')[0],
     token   = config.instagram.access_token;
 
 router.get('/', function (req, res) {
     request({
-        url: 'https://api.instagram.com/v1/users/' + user + '/media/recent?access_token=' + token,
+        url: 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token,
         json: true,
         timeout: 10000
     }, function (error, response, body) {
