bd7532fb227145c1ff185b4fd3f290f86625066d
Pedro Rogério <pinceladasdaweb@hotmail.com>
2016-03-27 16:44:07 -0300

    Migrated to request-promise

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,42 @@
 /*jslint node: true */
 "use strict";
 
-var express = require('express'),
-    request = require('request'),
-    moment  = require('moment'),
-    config  = require('../config/config.js'),
-    router  = express.Router(),
-    token   = config.instagram.access_token;
+var express  = require('express'),
+    rp       = require('request-promise'),
+    moment   = require('moment'),
+    config   = require('../config/config.js'),
+    router   = express.Router(),
+    username = config.instagram.username,
+    token    = config.instagram.access_token,
+    options  = {
+        uri: 'https://api.instagram.com/v1/users/search',
+        qs: {
+            q: username,
+            access_token: token
+        },
+        json: true
+    };
 
 router.get('/', function (req, res) {
-    request({
-        url: 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token,
-        json: true,
-        timeout: 10000
-    }, function (error, response, body) {
-        res.render('index', {
-            config  : config,
-            moment  : moment,
-            body    : body
+    rp(options)
+        .then(function (res) {
+            var id = res.data[0].id;
+
+            return rp({
+                uri: 'https://api.instagram.com/v1/users/' + id + '/media/recent/?access_token=' + token,
+                json: true
+            });
+        })
+        .then(function (json) {
+            res.render('index', {
+                config  : config,
+                moment  : moment,
+                body    : json
+            });
+        })
+        .catch(function (err) {
+            res.send(err);
         });
-    });
 });
 
 module.exports = router;
